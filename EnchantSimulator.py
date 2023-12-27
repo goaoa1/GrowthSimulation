927bb222faf298bb6b56d406a50c6016f5451c10
goaoa <goaoa@naver.com>
2023-12-27 20:52:38 +0900

    gogogo

--- EnchantSimulator.py
+++ EnchantSimulator.py
@@ -49,12 +49,13 @@ def getBinomialDistribution(
     input()
 
 
+# 목표 강화 수치 이상 달성 확률을 계산한다. get_probability_of_reaching_target
 # 목표 강화 수치 이상 달성시 스탑
-def getRateOfEnchantLevel(
+def getRateOfReachingEnchantLevel(
     enchantTable,
     currentEnchantLevel,
     lowerLimitEnchantLevel,
-    try_count,
+    max_try_count,
     targetEnchantLevel,
 ):
     success_rate = enchantTable[currentEnchantLevel]["success_rate"]
@@ -70,7 +71,7 @@ def getRateOfEnchantLevel(
 
     for simulation in range(simulation_count):
         _currentEnchantLevel = currentEnchantLevel
-        for tryout in range(try_count):
+        for tryout in range(max_try_count):
             tempRandom = random.random()
             if success_rate >= tempRandom:
                 _currentEnchantLevel += successReward
@@ -94,24 +95,3 @@ def getRateOfEnchantLevel(
             result_table[_currentEnchantLevel] = 1
     print(result_table.items())
     input()
-
-
-def calculateTargetEnchantLevelSuccessPercent(
-    enchantType, try_Count, targetEnchantLevel, success_Percent
-):
-    # TODO 아래는 뒷걸음칠 곳이 무한대일 때의 확률이다. 0강 이하로 내려가지 않는 경우에는 다른 확률이 필요하다!
-    if enchantType == "penalty1":
-        # 검증하는 곳(짝수 + 짝수 또는 홀수 + 홀수 쌍만 나오도록 검증)
-        if try_Count % 2 == 0:
-            if targetEnchantLevel % 2 != 0:
-                raise Exception("시도 횟수가 짝수면 목표 강화 횟수도 짝수여야 합니다")
-        else:
-            if targetEnchantLevel % 2 != 1:
-                raise Exception("시도 횟수가 홀수면 목표 강화 횟수도 홀수여야 합니다")
-
-        combination_Count = comb(try_Count - 2, (try_Count + targetEnchantLevel) / 2)
-        return (
-            success_Percent ** ((try_Count + targetEnchantLevel) / 2)
-            * (1 - success_Percent) ** ((try_Count - targetEnchantLevel) / 2)
-            * combination_Count
-        )
